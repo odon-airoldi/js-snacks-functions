1aac1fa7cd06cd1e428b77372a0de1c500dbc11a
odon-airoldi <odon.airoldi@gmail.com>
2026-02-10 16:13:24 +0100

    completed exercise 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,28 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi
+e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function get_inits_words(arr_words) {
+
+    const arr_inits_words = []
+
+    for(i = 0; i < arr_words.length; i++) {
+
+        const inits_words = arr_words[i].substring(0, 1)
+
+        arr_inits_words.push(inits_words)
+
+    }
+
+    return arr_inits_words
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(get_inits_words(names))
 
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+// Risultato atteso: ["A", "L", "M", "A", "G", "A"]
