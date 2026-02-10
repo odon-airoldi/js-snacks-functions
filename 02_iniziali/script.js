8237e411e45a2a40874067c01fc6ae754d4f535b
odon-airoldi <odon.airoldi@gmail.com>
2026-02-10 23:35:46 +0100

    fix exercise 02_iniziali loop for index

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -27,3 +27,25 @@ function get_inits_words(arr_words) {
 console.log(get_inits_words(names))
 
 // Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+// Arrow Functions Version
+
+const get_inits_words_ = (arr_words) => {
+
+    const arr_inits_words = []
+
+    for(i = 0; i < arr_words.length; i++) {
+
+        const inits_words = arr_words[i][0]
+
+        arr_inits_words.push(inits_words)
+
+    }
+
+    return arr_inits_words
+
+}
+
+console.log(get_inits_words_(names))
