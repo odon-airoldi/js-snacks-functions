25510e4bb3e2f565fd17a6394631dfbbff1bb71f
odon-airoldi <odon.airoldi@gmail.com>
2026-02-10 16:39:26 +0100

    completed exercise 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,32 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera
+e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function get_words_same_initial(arr_words, letter) {
+
+    const arr_words_same_initial = []
+
+    for(i = 0; i < arr_words.length; i++) {
+
+        const inits_words = arr_words[i].substring(0, 1)
+
+        if (inits_words === letter) {
+
+            arr_words_same_initial.push(arr_words[i])
+
+        }
+
+    }
+
+    return arr_words_same_initial
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(get_words_same_initial(names, 'A'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
